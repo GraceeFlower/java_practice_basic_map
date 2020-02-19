40ed8e2c77202ba7e4233509c18d6f3ecf1bd563
GraceeFlower <1491644103@qq.com>
2020-02-19 23:11:35 +0800

    [WM] feat: add the students to the teacher & print the students' info

--- map/src/main/java/Application.java
+++ map/src/main/java/Application.java
@@ -1,6 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
 
-  public static void main(String[] args) {
-  }
+    public static void main(String[] args) {
+        Student linda = new Student("Linda", 18);
+        Student cindy = new Student("Cindy", 19);
+        Student mary = new Student("Mary", 18);
+
+        Map<String, Integer> stuMap = new HashMap<>();
+        stuMap.put(linda.getName(), linda.getAge());
+        stuMap.put(cindy.getName(), mary.getAge());
+        stuMap.put(mary.getName(), mary.getAge());
+
+        Teacher teacher = new Teacher("Bob", stuMap);
+        teacher.addStudent(new Student("Ben", 19));
+        teacher.addStudent(new Student("Solider", 20));
+
+        teacher.printStudentInfo();
+    }
 
 }

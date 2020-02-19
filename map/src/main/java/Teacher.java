3f097fd4f3266a65f9eeff156cd6b88fa953d244
GraceeFlower <1491644103@qq.com>
2020-02-19 23:04:20 +0800

    [WM] feat: complete the Teacher Class

--- map/src/main/java/Teacher.java
+++ map/src/main/java/Teacher.java
@@ -1,3 +1,40 @@
+import java.util.Map;
+import java.util.Set;
+
 public class Teacher {
 
+    private String name;
+    private Map<String, Integer> stuMap;
+
+    public Teacher(String name, Map<String, Integer> stuMap) {
+        this.name = name;
+        this.stuMap = stuMap;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, Integer> getStuMap() {
+        return stuMap;
+    }
+
+    public void setStuMap(Map<String, Integer> stuMap) {
+        this.stuMap = stuMap;
+    }
+
+    public void addStudent(Student newStudent) {
+        this.stuMap.put(newStudent.getName(), newStudent.getAge());
+    }
+
+    public void printStudentInfo() {
+        Set<Map.Entry<String, Integer>> stuSet = stuMap.entrySet();
+        for(Map.Entry<String, Integer> i: stuSet) {
+            System.out.println("姓名：" + i.getKey() + " 年龄：" + i.getValue());
+        }
+    }
 }
